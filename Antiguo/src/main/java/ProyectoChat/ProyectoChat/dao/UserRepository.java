7dd77a9059d6edd1bf793e25900f207a10c63b32
FlaviaSalazar27 <flaviasalazarovalle@gmail.com>
2019-12-07 20:15:52 -0400

    Refactoring 2

--- Antiguo/src/main/java/ProyectoChat/ProyectoChat/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ProyectoChat.ProyectoChat.dao;
-
-import ProyectoChat.ProyectoChat.domain.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepository  extends JpaRepository<Usuario, Integer> {
-    Usuario findByBotUserId(String botUserId);
-}
