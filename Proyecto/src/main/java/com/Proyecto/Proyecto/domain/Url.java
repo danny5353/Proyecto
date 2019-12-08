855a0160a5b36dc3932721dac8fb8b247eb428ec
danny <dannyzambranasanabria@gmail.com>
2019-12-07 20:03:43 -0400

    Modificando base de datos

--- Proyecto/src/main/java/com/Proyecto/Proyecto/domain/Url.java
+++ Proyecto/src/main/java/com/Proyecto/Proyecto/domain/Url.java
@@ -1,6 +1,9 @@
-package com.Proyecto.Proyo.domain;
-
-import com.Proyecto.Proyecto.domain.Ruta;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Proyecto.Proyecto.domain;
 
 import java.io.Serializable;
 import java.util.Date;
